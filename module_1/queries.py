b652d892838274092ee6f68cb96baa912c39278b
leeannapugh10 <leeannapugh10@gmail.com>
2023-11-13 11:30:24 -0600

    adds a bigger quary and refactors python for connect to db into functions

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,2 +1,13 @@
 '''# step 3 - Write a query'''
 SELECT_ALL = 'SELECT character_id, name FROM charactercreator_character;'
+
+
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+SELECT cc_char.name, avg(ai.weight) AS avg_item_weight
+FROM charactercreator_character AS cc_char
+JOIN charactercreator_character_inventory AS cc_inv
+ON cc_char.character_id=cc_inv.character_id
+JOIN armory_item AS ai
+ON ai.item_id=cc_inv.item_id
+GROUP BY cc_char.character_id
+'''
--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,16 +1,30 @@
 '''
 sqlite example for sprint 10, mod 1
 '''
-# Step 0 - import sqlite3
 import sqlite3
 import queries as q
+import pandas as pd
+
+
+# DB Connect function
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    '''Connection function to for DB'''
+    return sqlite3.connect(db_name)
+
+
+def execute_q(conn, query):
+    '''function to make cursor and execute query and pulling results'''
+    curs = conn.cursor()
+    curs.execute(query)
+    return curs.fetchall()
+
 
 # step 1 - Connect to the database
 # triple check the spelling of your database filename
-connection = sqlite3.connect('rpg_db.sqlite3')
+# connection = sqlite3.connect('rpg_db.sqlite3')
 
 # step 2 - Make the "cursor"
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
 # step 3 - Write a query
 # QUERY = 'SELECT character_id, name FROM charactercreator_character;'
@@ -18,7 +32,13 @@ cursor = connection.cursor()
 
 # step 4 - Execute the query on the cursor and fetch results
 # "pulling the results" from the cursor
-results = cursor.execute(q.SELECT_ALL).fetchall()
+# results = cursor.execute(q.SELECT_ALL).fetchall()
 
 if __name__ == '__main__':
-    print(results[:5])
+    conn = connect_to_db()
+    print(execute_q(conn, q.SELECT_ALL))
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)
+    df = pd.DataFrame(results)
+    df.columns = ['name', 'average_item_weight']
+    print(df.head())
+    df.to_csv('rpg_db.csv', index=False)
